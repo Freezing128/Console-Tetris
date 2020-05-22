6a6fe8f54e6c25c0c25d93ef73ddd007ed76e81f
Freezing128 <luks.volf@seznam.cz>
2020-05-22 15:37:43 +0200

    Point class - stores information about each block's pixel

--- Point.cs
+++ Point.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/***
+ * C# Console application - Tetris remake
+ * @Author: Freezing
+ * @Date: 22/05/2020
+ * 
+ * Point.cs
+ */
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +13,12 @@ using System.Threading.Tasks;
 
 namespace Tetris
 {
+    /***
+     * Class for storing the information about each pixel ("point") of some given block
+     */
     class Point
     {
+        // Class properties
         private int pos_x;
         private int pos_y;
         private int state;
@@ -26,18 +37,21 @@ namespace Tetris
             get { return state; }
             set { state = value; }
         }
+        // Default constructor
         public Point()
         {
             pos_x = 0;
             pos_y = 0;
             state = 0;
         }
+        // Alternative constuctor #1
         public Point(int state, int x, int y)
         {
             pos_x = x;
             pos_y = y;
             this.state = state;
         }
+        // Alternative constuctor #2
         public Point(int state)
         {
             this.state = state;
